da93920c1adc99650abc3a88dadbcd8fcbff6e3d
deadPupsich <kibelousov@mail.ru>
2026-04-03 17:31:09 +0300

    compose dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,5 +17,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
